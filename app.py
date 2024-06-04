c793426390dc0c1c0407ffeca987435ee04a7fbb
David Lai <davidlai0263@gmail.com>
2024-06-05 02:26:31 +0800

    feat: Add SQLite database functionality

--- app.py
+++ app.py
@@ -4,12 +4,15 @@ from linebot.v3.messaging import (
     MessagingApi,
     ReplyMessageRequest,
     TextMessage,
+    MessageAction,
     LocationMessage,
     TemplateMessage,
     LocationAction,
     ButtonsTemplate,
     QuickReply,
-    QuickReplyItem
+    QuickReplyItem,
+    FlexMessage,
+    FlexContainer,
 )
 from linebot.v3.webhooks import (
     MessageEvent,
@@ -28,10 +31,250 @@ from argparse import ArgumentParser
 import sys
 import os
 from dotenv import load_dotenv
+import json
+import pandas as pd
+
+from database import Database
 
 load_dotenv()
 
 app = Flask(__name__)
+
+# Database
+db = Database('test.db')
+if not db.table_exists('my_table'):
+    db.create_table('my_table', ['id TEXT PRIMARY KEY', 'status TEXT', 'lat TEXT', 'lng TEXT'])
+    print('Table created.')
+quick_options = [
+    QuickReplyItem(
+        action=MessageAction(label="便當", text="便當")
+    ),
+    QuickReplyItem(
+        action=MessageAction(label="小吃店", text="小吃店")
+    ),
+    QuickReplyItem(
+        action=MessageAction(label="滷味", text="滷味")
+    ),
+    QuickReplyItem(
+        action=MessageAction(label="炸物", text="炸物")
+    ),
+    QuickReplyItem(
+        action=MessageAction(label="麵食", text="麵食")
+    ),
+    QuickReplyItem(
+        action=MessageAction(label="早餐", text="早餐")
+    ),
+    QuickReplyItem(
+        action=MessageAction(label="宵夜", text="宵夜")
+    ),
+]
+
+
+def cal_distance(lat1, lon1, lat2, lon2):
+    from math import sin, cos, sqrt, atan2, radians
+    # approximate radius of earth in km
+    R = 6373.0
+
+    lat1 = radians(lat1)
+    lon1 = radians(lon1)
+    lat2 = radians(lat2)
+    lon2 = radians(lon2)
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    distance = R * c
+
+    return distance
+
+def get_flex_message(name, star, reviews_count, distance, pros, cons, url):
+    flex_message = '''
+{
+  "type": "bubble",
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "Brown Cafe",
+        "weight": "bold",
+        "size": "xl"
+      },
+      {
+        "type": "box",
+        "layout": "baseline",
+        "margin": "md",
+        "contents": [
+          {
+            "type": "icon",
+            "size": "sm",
+            "url": "https://developers-resource.landpress.line.me/fx/img/review_gold_star_28.png"
+          },
+          {
+            "type": "icon",
+            "size": "sm",
+            "url": "https://developers-resource.landpress.line.me/fx/img/review_gold_star_28.png"
+          },
+          {
+            "type": "icon",
+            "size": "sm",
+            "url": "https://developers-resource.landpress.line.me/fx/img/review_gold_star_28.png"
+          },
+          {
+            "type": "icon",
+            "size": "sm",
+            "url": "https://developers-resource.landpress.line.me/fx/img/review_gold_star_28.png"
+          },
+          {
+            "type": "icon",
+            "size": "sm",
+            "url": "https://developers-resource.landpress.line.me/fx/img/review_gray_star_28.png"
+          },
+          {
+            "type": "text",
+            "text": "4.0",
+            "size": "sm",
+            "color": "#999999",
+            "margin": "md",
+            "flex": 0
+          },
+          {
+            "type": "text",
+            "text": "(123)",
+            "size": "sm",
+            "color": "#999999",
+            "margin": "md",
+            "flex": 0
+          }
+        ]
+      },
+      {
+        "type": "box",
+        "layout": "vertical",
+        "margin": "lg",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "box",
+            "layout": "baseline",
+            "spacing": "sm",
+            "contents": [
+              {
+                "type": "text",
+                "text": "距離",
+                "color": "#aaaaaa",
+                "size": "sm",
+                "flex": 1
+              },
+              {
+                "type": "text",
+                "text": "Flex Tower, 7-7-4 Midori-ku, Tokyo",
+                "wrap": true,
+                "color": "#666666",
+                "size": "sm",
+                "flex": 5
+              }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "baseline",
+            "spacing": "sm",
+            "contents": [
+              {
+                "type": "text",
+                "text": "優點",
+                "color": "#aaaaaa",
+                "size": "sm",
+                "flex": 1
+              },
+              {
+                "type": "text",
+                "text": "10:00 - 23:00",
+                "wrap": true,
+                "color": "#666666",
+                "size": "sm",
+                "flex": 5
+              }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "baseline",
+            "spacing": "sm",
+            "contents": [
+              {
+                "type": "text",
+                "text": "缺點",
+                "color": "#aaaaaa",
+                "size": "sm",
+                "flex": 1
+              },
+              {
+                "type": "text",
+                "text": "10:00 - 23:00",
+                "wrap": true,
+                "color": "#666666",
+                "size": "sm",
+                "flex": 5
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  "footer": {
+    "type": "box",
+    "layout": "vertical",
+    "spacing": "sm",
+    "contents": [
+      {
+        "type": "button",
+        "style": "link",
+        "height": "sm",
+        "action": {
+          "type": "uri",
+          "label": "開啟 Google Map 導航",
+          "uri": "https://line.me/"
+        }
+      }
+    ],
+    "flex": 0
+  }
+}
+'''
+    gold_star = "https://developers-resource.landpress.line.me/fx/img/review_gold_star_28.png"
+    gray_star = "https://developers-resource.landpress.line.me/fx/img/review_gray_star_28.png"
+    json_data = json.loads(flex_message)
+    json_data['body']['contents'][0]['text'] = name
+    for i in range(5):
+        if i < int(star):
+            json_data['body']['contents'][1]['contents'][i]['url'] = gold_star
+        else:
+            json_data['body']['contents'][1]['contents'][i]['url'] = gray_star
+    json_data['body']['contents'][1]['contents'][5]['text'] = str(star)
+    json_data['body']['contents'][1]['contents'][6]['text'] = str(reviews_count)
+    json_data['body']['contents'][2]['contents'][0]['contents'][1]['text'] = distance
+    json_data['body']['contents'][2]['contents'][1]['contents'][1]['text'] = pros
+    json_data['body']['contents'][2]['contents'][2]['contents'][1]['text'] = cons
+    json_data['footer']['contents'][0]['action']['uri'] = url
+    return json_data
+
+def get_carousel_message(flex_message_list):
+    flex_message = '{"type": "carousel","contents": ['
+    flex_message = {
+        "type": "carousel",
+        "contents": flex_message_list
+    }
+    return str(json.dumps(flex_message))
+
+# init shop data
+shop_data = pd.read_csv('result.csv')
 
 channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
 channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
@@ -71,25 +314,71 @@ def callback():
 def handle_message(event):
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
-        buttons_template = ButtonsTemplate(
+        
+        # 檢查是否已經存在該記錄
+        user_id = event.source.user_id
+        print(user_id)
+        db_data = db.read_data('my_table')
+        print(db_data)
+        if db_data != []:
+            result = [data for data in db_data if data[0] == user_id]
+            if result == []:
+                db.insert_data('my_table', (user_id, 0, '', ''))
+                result = [data for data in db_data if data[0] == user_id]
+            user_data = result[0]
+        else:
+            db.insert_data('my_table', (user_id, 0, '', ''))
+        
+        
+        
+        print(user_data)
+        msg = [] # 回覆訊息
+
+        if user_data[1] == '2': # 2: 重置
+            print('reset user data')
+            db.insert_data('my_table', (user_id, 0, '', ''))
+            user_data = db.read_data('my_table')[0]
+        
+
+        if user_data[1] == '0': # 0: 未選擇位置
+            print('user not select location')
+            buttons_template = ButtonsTemplate(
             text='請在地圖上選擇所在位置',
             actions=[
-                LocationAction(
-                    label='傳送位置',
-                )
-            ]
-        )
-
-        # 回覆訊息
-        line_bot_api.reply_message_with_http_info(
-            ReplyMessageRequest(
-                reply_token=event.reply_token,
-                messages=[TemplateMessage(
-                    alt_text='請在地圖上選擇所在位置',
-                    template=buttons_template),
+                    LocationAction(
+                        label='傳送位置',
+                    )
                 ]
             )
-        )
+            msg.append(TextMessage(text='你還沒告訴我你的位置呦！'))
+            msg.append(TemplateMessage(alt_text='請在地圖上選擇所在位置',
+                    template=buttons_template)),
+        elif user_data[1] == '1': # 1: 未選擇類型
+            print('user not select type')
+            if event.message.text in ['便當', '小吃店', '滷味', '炸物', '麵食', '早餐', '宵夜']:
+                db.insert_data('my_table', (user_id, 2, user_data[2], user_data[3]))
+                flex_message = get_carousel_message([get_flex_message('Brown Cafe', 4, 123, '1.2km', '10:00 - 23:00', '10:00 - 23:00', 'https://line.me/'), get_flex_message('Brown Cafe', 4, 123, '1.2km', '10:00 - 23:00', '10:00 - 23:00', 'https://line.me/')])
+                print(flex_message)
+                msg.append(TextMessage(text='好的！我們來幫你找吃的！'))
+                msg.append(
+                    FlexMessage(
+                        alt_text='Brown Cafe',
+                        contents=FlexContainer.from_json(flex_message)
+                    )
+                )
+            else:
+                quick_reply = QuickReply(
+                    items=quick_options
+                )
+                msg.append(TextMessage(text='還沒告訴我你想吃哪種類型的呦！', quickReply=quick_reply))
+        if msg != []:
+            # 回覆訊息
+            line_bot_api.reply_message_with_http_info(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    messages=msg
+                )
+            )
 
 
 @handler.add(MessageEvent, message=LocationMessageContent)
@@ -97,18 +386,25 @@ def message_location(event):
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
         print(event.message)
+
+        user_id = event.source.user_id
+        db.insert_data('my_table', (user_id, 1, event.message.latitude, event.message.longitude))
+        db_data = db.read_data('my_table')
+        for i in db_data:
+            if i[0] == user_id:
+                user_data = i
+                break
+
+        print(user_data)
+
         quick_reply = QuickReply(
-            items=[
-                QuickReplyItem(
-                    action=LocationAction(label="傳送位置")
-                )
-            ]
+            items=quick_options
         )
         line_bot_api.reply_message_with_http_info(
             ReplyMessageRequest(
                 reply_token=event.reply_token,
                 messages=[TextMessage(
-                    text="請問您還需要什麼服務嗎？", quickReply=quick_reply)]
+                    text="請問你想吃哪種類型的呢？", quickReply=quick_reply)]
             )
         )
 
---
+++ database.py
@@ -0,0 +1,67 @@
+import sqlite3
+import threading
+
+class Database:
+    def __init__(self, db_file):
+        self.db_file = db_file
+        self.local = threading.local()
+
+    @property
+    def conn(self):
+        if not hasattr(self.local, 'conn'):
+            self.local.conn = sqlite3.connect(self.db_file)
+        return self.local.conn
+
+    @property
+    def cursor(self):
+        if not hasattr(self.local, 'cursor'):
+            self.local.cursor = self.conn.cursor()
+        return self.local.cursor
+
+    def create_table(self, table_name, columns):
+        try:
+            column_defs = ', '.join(columns)
+            self.cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} ({column_defs})")
+            self.conn.commit()
+            print(f"Table '{table_name}' created or already exists.")
+        except sqlite3.Error as e:
+            print(f"Error creating table: {e}")
+
+    def insert_data(self, table_name, data):
+        try:
+            # 檢查是否已經存在該記錄
+            self.cursor.execute(f"SELECT COUNT(*) FROM {table_name} WHERE id = ?", (data[0],))
+            count = self.cursor.fetchone()[0]
+            
+            if count > 0:
+                # 更新記錄
+                self.cursor.execute(f"UPDATE {table_name} SET status = ?, lat = ?, lng = ? WHERE id = ?", data[1:] + (data[0],))
+            else:
+                # 插入新記錄
+                placeholders = ', '.join(['?' for _ in range(len(data))])
+                self.cursor.execute(f"INSERT INTO {table_name} VALUES ({placeholders})", data)
+            
+            self.conn.commit()
+            return True
+        except sqlite3.Error as e:
+            print(f"Error inserting/updating data: {e}")
+            return False
+
+    def read_data(self, table_name):
+        try:
+            self.cursor.execute(f"SELECT * FROM {table_name}")
+            return self.cursor.fetchall()
+        except sqlite3.Error as e:
+            print(f"Error reading data: {e}")
+            return []
+
+    def table_exists(self, table_name):
+        try:
+            self.cursor.execute(f"SELECT COUNT(*) FROM sqlite_master WHERE type='table' AND name='{table_name}'")
+            return self.cursor.fetchone()[0] > 0
+        except sqlite3.Error as e:
+            print(f"Error checking table existence: {e}")
+            return False
+
+    def close_connection(self):
+        self.conn.close()
